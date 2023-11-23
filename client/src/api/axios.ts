e3068f713bcb83fa5a886543d67eb496c7117d79
kaneki081 <joseacebuche2@gmail.com>
2023-11-23 18:12:56 +0800

    3rd commit

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const localBaseUrl = "http://localhost:4200/api/v1";
-// const productionBaseUrl = "https://websitename.com";
+// const localBaseUrl = "http://localhost:4200/api/v1";
+const productionBaseUrl = "https://prisma-proj-server.vercel.app/api/v1";
 
 export default axios.create({
-  baseURL: localBaseUrl,
+  baseURL: productionBaseUrl,
   withCredentials: true,
 });
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prisma-proj-client.vercel.app/",
     credentials: true,
   })
 );
